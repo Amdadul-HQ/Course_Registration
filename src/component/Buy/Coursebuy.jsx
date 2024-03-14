55befa1f2b0fbf408b5c797210a86e44aac27b58
AmdadulHQ <rimonamdadul301@gmail.com>
2024-03-14 17:37:59 +0600

    adding course select section

--- src/component/Buy/Coursebuy.jsx
+++ src/component/Buy/Coursebuy.jsx
@@ -1,25 +1,31 @@
 
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Coursebuy = ({selectCourses}) => {
+const Coursebuy = ({selectCourses,credit,remainingCredit,totalPrice}) => {
+
+    
+
+    // console.log(totalCredit);
+
 
     return (
         <div className='bg-white h-fit w-1/5 py-5 px-2'>
-            <h3 className='text-[#2F80ED] text-lg font-bold border-b-2 py-2'>Credit Hour Remaining 7 hr</h3>
+            <h3 className='text-[#2F80ED] text-lg font-bold border-b-2 py-2'>Credit Hour Remaining {remainingCredit} hr</h3>
             <h3 className='text-[#1C1B1B] text-xl font-bold mb-6'>Course Name</h3>
-            <ol className='pb-6 border-b-2 mb-4'>
+            <ol className='pb-6 border-b-2 mb-4 list-decimal ml-3'>
                 {
-                    selectCourses.map(course => <li> {course.title}</li>)
+                    selectCourses.map(course => <li key={course.id}>  {course.title}</li>)
                 }
             </ol>
-            <p className='text-[rgba(28,27,27,0.80)] text-base font-medium pb-5 border-b-2'>Total Credit Hour : 13</p>
-            <p className='text-[rgba(28,27,27,0.80)] text-base font-semibold py-5'>Total Price : 48000 USD</p>
+            <p className='text-[rgba(28,27,27,0.80)] text-base font-medium pb-5 border-b-2'>Total Credit Hour : {credit}</p>
+            <p className='text-[rgba(28,27,27,0.80)] text-base font-semibold py-5'>Total Price : {totalPrice} USD</p>
         </div>
     );
 };
 
 Coursebuy.propTypes = {
-    
+    selectCourses:PropTypes.array.isRequired,
 };
 
 export default Coursebuy;
--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -5,17 +5,31 @@ import Coursebuy from '../Buy/Coursebuy';
 import { useState } from 'react';
 
 const Main = () => {
-    const [selectCourses,setSelectCourses] = useState([])
+    const [selectCourses,setSelectCourses] = useState([]);
+    const [credit,setCredit] =useState(0);
+    const [remainingCredit,setRemainingCredit] = useState(10);
+    const [totalPrice,setTotalPrice] = useState(0);
+
+    
 
     const handleSelectCourse = (coures) => {
+        if(selectCourses.includes(coures)){
+            return alert('You Can Not Select A Course Twice')
+        }
         const addCourses = [...selectCourses,coures]
+        
         setSelectCourses(addCourses)
+        
+        setCredit(credit + coures.credit)
+        setRemainingCredit(remainingCredit - coures.credit)
+        setTotalPrice(totalPrice + coures.price)
+        
     }
 
     return (
         <main className='max-w-screen-xl mx-auto flex gap-x-4'>
-            <Courses handleSelectCourse={handleSelectCourse} ></Courses>
-            <Coursebuy selectCourses={selectCourses} ></Coursebuy>
+            <Courses handleSelectCourse={handleSelectCourse}></Courses>
+            <Coursebuy selectCourses={selectCourses} credit={credit} remainingCredit={remainingCredit} totalPrice={totalPrice} ></Coursebuy>
         </main>
     );
 };
